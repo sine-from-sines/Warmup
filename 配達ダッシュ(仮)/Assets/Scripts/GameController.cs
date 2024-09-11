bcf68a136928fe406cb66909147d3ecf6e011e0d
sign_from_sines <lsa2dsa5dr@gmail.com>
2024-09-11 15:27:48 +0900

    シーン追加、GameControllerに変数ちょっと追加(仮)　その他プロジェクト設定調整

--- 配達ダッシュ(仮)/Assets/Scripts/GameController.cs
+++ 配達ダッシュ(仮)/Assets/Scripts/GameController.cs
@@ -5,9 +5,19 @@ using UnityEngine;
 public class GameController : MonoBehaviour
 {
     //! このオブジェクトを2つ以上生成しない
-    [SerializeField] static GameController instance = null;
+    static GameController instance = null;
     //! スコア
     [SerializeField] int score = 0;
+    //! 1mごとに加算されるスコア量
+    [SerializeField] const int scorePerMeters = 10;
+    //! 速度初期値
+    [SerializeField] const float speed = 1.0f;
+    //! 速度、一定距離ごとの加速度
+    [SerializeField] const float speedAccel = 0.1f;
+    //! 速度、加速する頻度
+    [SerializeField] const float speedAccelFreq = 10.0f;
+    //! 速度最大値
+    [SerializeField] const float speedMax = 10.0f;
 
     //! ハイスコア記録(仮で10位まで)
     [SerializeField] int[] scoreRanking = new int[10];
@@ -17,6 +27,7 @@ public class GameController : MonoBehaviour
         if (instance == null)
         {
             instance = this;
+            Debug.Log("Game Started!");
             DontDestroyOnLoad(this.gameObject);
         }
         else
